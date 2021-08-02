bdcca631f971aab3421726a62888f76b2906b9b5
texasrachel <texasrachel@gmail.com>
2021-08-02 15:42:29 -0500

    updated for Monday, with notes added

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+//https://forums.tumult.com/t/block-rotation-smartphone/6362/2
+// /if (window.innerHeight < window.innerWidth) { // portrait orientation
+//   // show a message that asks the user to use portrait mode
+//   myElement = document.getElementById('ELEMENT_ID')
+//   myElement.innerHTML = "Please use Portrait Orientation";
+// }
