047e3ae5e148a97678936bc5d1a954b80a520a3e
texasrachel <texasrachel@gmail.com>
2021-08-04 15:39:59 -0500

    wednesday work

--- test.js
+++ test.js
@@ -1,99 +1,138 @@
 //temporary code
 
 // const backgroundColor = document.createElement('div')
-// const canvas = document.createElement('img')
 // const localStorage = window.localStorage
 
-const container = document.getElementById('container')
-const openingImg = document.getElementById('image')
-const input = document.getElementById('input')
-const inputButton = document.getElementById('input-button')
+// const container = document.getElementById('container')
+const openingImg = document.getElementById('background-image')
+const generate = document.getElementById('generate')
+const generateButton = document.getElementById('generate-button')
 const resetButton = document.getElementById('reset-button')
 const startOver = document.getElementById('start-over')
 
+//wallpaper generation 
 //https://www.javascripttutorial.net/web-apis/javascript-canvas/
-const canvas = document.getElementById('wallpaper')
-const wallpaperWidth = canvas.width
-const wallpaperHeight = canvas.height
+//https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+// const newWallpaper = document.getElementById('wallpaper')
+
 const colorArray = ['ee9b00', '005f73']
-const wallpaper = document.createElement('a')
+const fontArray = ['Shadows Into Light', 'Alex Brush']
 const quoteArray = ['You are the architect', 'We must also do']
+const quote = "The Price of Anything"
+//size elements
 
 //functions
+// https://stackoverflow.com/questions/6887183/how-to-take-screenshot-of-a-div-with-javascript
+function test(width, height) {
+  const smallWallpaper = document.getElementById('wallpaperSmall')
+  let context = smallWallpaper.getContext('2d')
+  context.fillRect(0, 0, 500, 800)
+  context.font = "48px cursive"
+  context.fillStyle = '#ffa500'
+  context.textAlign = "center"
+  context.textBaseline = 'middle'
+  context.fillText = (quote, 250, 250)
+  
+  return smallWallpaper
+}
+
+generateButton.addEventListener("click", () => {
+  test()
+  generate.style.display = 'none'
+  openingImg.style.display = 'none'
+  // let newWallpaper = smallWallpaper.toDataURL("image/png")
+  // document.write('<img src="'+newWallpaper+'"/>>')
+})
 
 function randomizeArray(array) {
   let length = array.length
   index = Math.floor(Math.random() * length)
 }
 
+function save(image, imageName) {
+  let link = document.createElement('a')
+  link.download = imageName
+  link.href = 'data:', + image
+  link.click()
+}
+
+
+// function createWallpaper() {
+//   const test = document.querySelector('smallWallpaper')
+//   let ctx = test.getContext('2d')
+//   ctx.fillStyle = 'red'
+//   ctx.fillRect(10, 10, 100, 100)
+// }
+
+// function createWallpaper() {
+//   // if (smallWallpaper.getContext) {
+//    let context = smallWallpaper.getContext('2d')
+//    smallWallpaper.height = 400
+//     smallWallpaper.width = 300
+//     let randomColor = randomizeArray(colorArray)
+//     // context.fillStyle = randomColor
+//     context.fillStyle = 'red'
+
+//     //add overlay
+//     // context.fillRect(25, 25, 200, 200)
+//     // context.strokeRect(50, 50, 100, 100)
+
+//     // get quote
+//     let quote = "The Price of Anything"
+//     // let quote = randomizeArray(quoteArray)
+//     context.fillText = (quote, 0, 0)
+
+//     //quote in font
+//     // let font = randomizeArray(fontArray)
+//     // smallWallpaper.style.fontFamily = "Rochester"
+//     context.font = "48px Arial"
+
+//     context.textAlign = "center"
+//     context.textBaseline = 'bottom'
+
+//     //add quote
+//     smallWallpaper.append(quote)
+
+//     wallpaper.download = 'image.png'
+//     // wallpaper.href = imageURL
+
+//     smallWallpaper.append(wallpaper)
+//     // smallWallpaper.click()
+//     // smallWallpaper.removeChild(wallpaper)
+
+//     return smallWallpaper.toDataURL ('image/jpeg')
+// //   } else {
+// //     alert("Your device does not support this app. The quote of was:")
+// //   }
+// }
+
+// generateButton.addEventListener('click', () => {
+  // smallWallpaper.style.fontFamily = "Lovers Quarrel"
+  // let quote = "The world breaks everyone"
+  // smallWallpaper.style.display = 'block'
+  // startOver.style.display = "block"
+  // generate.style.display = "none"
+  // createWallpaper()
+//   test(500, 800)
+  // smallWallpaper.style.height = 100vh
+  // openingImg.style.display = "none"
+//   window.open('', smallWallpaper.toDataURL())
+// })
+
+// resetButton.addEventListener('click', function () {
+//   wallpaper.style.display = "none"
+//   smallWallpaper.style.display = 'none'
+//   startOver.style.display = "none"
+//   generate.style.display = "block"
+//   openingImg.style.display = "block"
+// })
+
 // function create() {
-//   canvas.style.background = "#ffd700"
-//   canvas.style.backgroundSize = "100%"
+//   smallWallpaper.style.background = "#ffd700"
+//   smallWallpaper.style.backgroundSize = "100%"
 // }
 
 //output color in mobile size
 
-// canvas.style.maxHeight = "100%"
-// canvas.style.width = "500px"
-
-function createWallpaper() {
-  let context = canvas.getContext('2d')
-  if (canvas.getContext) {
-    canvas.height = 400
-    canvas.width = 300
-    // let randomColor = randomizeArray(colorArray)
-    // context.fillRect(25, 25, 200, 200)
-    // context.strokeRect(50, 50, 100, 100)
-    context.fillStyle = "b22222"
-    context.fillText = ("Quote time", 0, 0)
-    context.font = "48px Arial"
-    context.textAlign = "center"
-    context.textBaseline = 'bottom'
-    
-    wallpaper.download = 'image.png'
-    // wallpaper.href = imageURL
-
-    canvas.append(wallpaper)
-    // canvas.click()
-    // canvas.removeChild(wallpaper)
-
-    return canvas.toDataURL ('image/jpeg')
-  } else {
-    alert("Your device does not support this app")
-  }
-}
-
-//add overlay
-
-//get quote
-let quote = "The Price of Anything"
-//let quote = randomizeArray(quoteArray)
-
-//quote in font
-canvas.style.fontFamily = "Rochester"
-
-//add quote
-canvas.append(quote)
-
-let newWallpaper = new Image(wallpaperWidth, wallpaperHeight)
-
-// newWallpaper.src = 
-
-inputButton.addEventListener('click', function () {
-  // canvas.style.fontFamily = "Lovers Quarrel"
-  let quote = "The world breaks everyone"
-  canvas.style.display = 'block'
-  startOver.style.display = "block"
-  input.style.display = "none"
-  createWallpaper()
-  // canvas.style.height = 100vh
-  openingImg.style.display = "none"
-})
-
-resetButton.addEventListener('click', function () {
-  wallpaper.style.display = "none"
-  canvas.style.display = 'none'
-  startOver.style.display = "none"
-  input.style.display = "block"
-  openingImg.style.display = "block"
-})
+// smallWallpaper.style.maxHeight = "100%"
+// smallWallpaper.style.width = "500px"
