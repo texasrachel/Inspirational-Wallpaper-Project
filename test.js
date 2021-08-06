31cb154a9fd2b3a11bf1ef55e75742839b1b7c3c
texasrachel <texasrachel@gmail.com>
2021-08-05 19:54:37 -0500

    promises pending needs fixed

--- test.js
+++ test.js
@@ -8,15 +8,31 @@ const fontArray = ['Shadows Into Light', 'Alex Brush']
 const hiddenDiv = document.getElementById('quote-hidden')
 
 const apiURL = "https://zenquotes.io/api/quotes/"
-const quoteURL = "https://zenquotes.io/api/random/84dcbeb778e17abd111cc16ad76e1e4caac13370?&a"
+const quoteURL = "https://zenquotes.io/api/random/84dcbeb778e17abd111cc16ad76e1e4caac13370?"
+
+const wallpaper = document.getElementById("wallpaper");
+const context = wallpaper.getContext('2d')
+let wallWidth = wallpaper.width
+let wallHeight = wallpaper.height
+let x = wallWidth / 4
+let y = wallHeight / 4
+let maxWidth = wallWidth * 0.9
+const lineHeight = 30
+let xStart = (wallWidth - maxWidth) / 2
+let yStart = 30
+
+const [a, b] = fontArray
+console.log(a);
+console.log(b);
 
 //wallpaper generation 
-//https://www.javascripttutorial.net/web-apis/javascript-canvas/
-//https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+// https://www.javascripttutorial.net/web-apis/javascript-canvas/
+// https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+// https://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
 
 //functions
 
-const getQuote = async () => {
+async function getQuote() {
   // removeElement(wallpaper)
   try {
     const response = await axios.get(quoteURL)
@@ -25,28 +41,67 @@ const getQuote = async () => {
       const quoteInd = document.createElement('p')
       quoteInd.textContent = element.q
       hiddenDiv.append(quoteInd)
+      quoteInd.setAttribute('id', 'quote-p')
       const author = document.createElement('p')
       author.textContent = element.a
       hiddenDiv.append(author)
+      author.setAttribute('id', 'author-p')
       const blockquote = document.createElement('p')
       blockquote.innerHTML = element.h
       hiddenDiv.append(blockquote)
-      // console.log(author)
-      // console.log(quoteInd)
+      blockquote.setAttribute('id', 'blockquote-p')
     })
-console.log(hiddenDiv)
-    } catch (error) {
+  } catch (error) {
       console.error(error)
   }
 }
-console.log(getQuote)
-const indiv = async () => {
-  const ind = await getQuote()
-  // console.log(Object.entries(indiv))
-}
-indiv()
+    console.log(getQuote())
+    let quote = getQuote()
 
-//http://www.java2s.com/Tutorials/HTML_CSS/HTML5_Canvas_Reference/strokeText.htm
+// const getQuote = async () => {
+//   // removeElement(wallpaper)
+//   try {
+//     const response = await axios.get(quoteURL)
+//     console.log(response.data);
+//     response.data.forEach((element) => {
+//       const quoteInd = document.createElement('p')
+//       quoteInd.textContent = element.q
+//       hiddenDiv.append(quoteInd)
+//       quoteInd.setAttribute('id', 'quote-p')
+//       const author = document.createElement('p')
+//       author.textContent = element.a
+//       hiddenDiv.append(author)
+//       author.setAttribute('id', 'author-p')
+//       const blockquote = document.createElement('p')
+//       blockquote.innerHTML = element.h
+//       hiddenDiv.append(blockquote)
+//       blockquote.setAttribute('id', 'blockquote-p')
+//       // console.log(author)
+//       // console.log(quoteInd)
+//     })
+// // console.log(hiddenDiv)
+    // console.log(document.getElementById('author-p').innerText)
+    // console.log(typeof 'author-p')
+
+  //   } catch (error) {
+  //     console.error(error)
+  // }
+  // const quote = document.getElementById('author-p').innerText
+
+// let quote = "Hello animals and others"
+// console.log(document.getElementById('author-p').innerText)
+// console.log(quote)
+// let quoteLength = quote.split(' ').length
+let quoteLength = 3
+// console.log(quoteLength);
+
+// const indiv = async () => {
+//   const ind = await getQuote()
+//   // console.log(Object.entries(indiv))
+// }
+// indiv()
+
+// http://www.java2s.com/Tutorials/HTML_CSS/HTML5_Canvas_Reference/strokeText.htm
 
 function reset() {
   let context = wallpaper.getContext('2d')
@@ -58,7 +113,6 @@ function reset() {
   // while (element.lastChild) {
   //   element.removeChild(element.lastChild)}
 }
-https://www.html5canvastutorials.com/tutorials/html5-canvas-wrap-text-tutorial/
 
 function wrap(context, quote, wallWidth, wallHeight, maxWidth, lineHeight) {
   //split quote for wrapping
@@ -88,25 +142,6 @@ function wrap(context, quote, wallWidth, wallHeight, maxWidth, lineHeight) {
   return x
   console.log(x);
 }
-
-const wallpaper = document.getElementById("wallpaper");
-const context = wallpaper.getContext('2d')
-const wallWidth = wallpaper.width
-let wallHeight = wallpaper.height
-let x = wallWidth / 4
-let y = wallHeight / 4
-let maxWidth = wallWidth * 0.9
-const lineHeight = 30
-const xStart = (wallWidth - maxWidth) / 2
-let yStart = 30
-let quote = "Hello animals and others"
-let quoteLength = quote.split(' ').length
-console.log(quoteLength);
-// context.font = "20px Arial"
-// context.fillStyle = "gold"
-const [a, b] = fontArray
-console.log(a);
-console.log(b);
 
 function wall() {
   let randomColor = colorArray[Math.floor(Math.random() * colorArray.length)]
